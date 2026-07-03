62ea8f8d57070de9a576f045383d7fae9ac49ae7
Felipe de Souza <131505159+Felipesouza28@users.noreply.github.com>
2026-07-02 21:25:56 -0300

    Add files via upload

--- projeto_final_UC2.py
+++ projeto_final_UC2.py
@@ -1,3 +1,5 @@
+from traceback import print_tb
+
 estoque_produtos = {
     1: {"nome": "Monitor Gamer LG UltraGear™ Curvo 34P", "preco": 2049.00, "quantidade": 20},
     2: {"nome": "Teclado Gamer Magnético Akko Monsgeek Fun60 Pro", "preco": 263.84, "quantidade": 80},
@@ -6,7 +8,7 @@ estoque_produtos = {
     5: {"nome": "SSD SATA A400, Kingston 240G", "preco": 360.00, "quantidade": 25},
     6: {"nome": "SSD Kingston NV3 1TB M.2 2280 NVMe", "preco": 849.90, "quantidade": 15}
 }
-
+subtotal = 0
 carrinho = []
 
 import time
@@ -22,7 +24,7 @@ while True:
     print("[4] Finalizar Compra")
     print("[0] Sair do Sistema")
 
-    opcao = int(input("\nEscolha uma opção: \n"))
+    opcao = int(input("\nEscolha uma opção: "))
     if opcao == 1:
         print("Visualizando estoque!")
         print("ID |                 Nome                |   Valor   | Quantidade")
@@ -31,22 +33,49 @@ while True:
 
     elif opcao == 2:
         print("Adicionando itens ao carrinho!")
-        for k,v in estoque_produtos.items():
-            print(f"{k} | {v["nome"]} | {v["preco"]} | {v["quantidade"]} ")
-            id_produto = int(input("Qual o id do produto que deseja comprar? "))
-            if id_produto in estoque_produtos:
-                qtd_produto = int(input("Quantas unidades voce deseja?"))
-                if qtd_produto <=0:
-                    print("Quantidade inválida")
-                elif qtd_produto <= estoque_produtos[id_produto]["quantidade"]:
-                    carrinho.append(estoque_produtos[id_produto])
-                    estoque_produtos[id_produto]["quantidade"] -= qtd_produto
+        id_produto = int(input("Qual o id do produto que deseja comprar? "))
+        if id_produto in estoque_produtos:
+            qtd_produto = int(input("Quantas unidades voce deseja?"))
+            if qtd_produto <=0:
+                print("Quantidade inválida")
+            elif qtd_produto <= estoque_produtos[id_produto]["quantidade"]:
+                item = {
+                    "qtd" : qtd_produto,
+                    "nome" : estoque_produtos[id_produto]["nome"],
+                    "preco" : estoque_produtos[id_produto]["preco"],
+                    "preco_total" : qtd_produto * estoque_produtos[id_produto]["preco"]
+                }
+
+                carrinho.append(item)
+                estoque_produtos[id_produto]["quantidade"] -= qtd_produto
+                print(f"Quantidade: {item['qtd']}, Nome: {item['nome']}, Preço: {item['preco']}")
+
+            else:
+                print(f"Quantidade índisponivel, temos apenas {estoque_produtos[id_produto]["quantidade"]} no estoque")
+
+        else:
+            print()
 
     elif opcao == 3:
         print("Visualizando carrinho!")
+        for i in carrinho:
+            print(f"{i["qtd"]}x {i["nome"]} no valor de R${i["preco"]}(cada)\nTotal R${i["preco_total"]} ")
+            subtotal += i["preco_total"]
+        print(f"Subtotal da Compra R${subtotal}")
 
     elif opcao == 4:
         print("Finalizando compras!")
+        cupom = input("Digite aqui um cupom válido: ").upper()
+        if cupom == "DEV10":
+            if subtotal >= 100:
+                desconto1 = subtotal * 0.1
+                print(f"Você obteve um desconto de 10%, totalizando R${desconto1:.2f} 😊")
+        elif cupom == "DEV20":
+            if subtotal >= 500:
+                desconto2 = subtotal * 0.2
+                print(f"Você obteve um desconto de 10%, totalizando R${desconto2:.2f} 😁")
+        else:
+            print("Você não obteve nenhum desconto 🥲")
 
     elif opcao == 0:
         print("Saindo do Site")
